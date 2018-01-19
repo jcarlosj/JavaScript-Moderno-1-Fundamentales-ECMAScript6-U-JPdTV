bdfa9e0d82285063d5bef77d090fb26a2bb9aaed
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-19 10:50:47 -0500

    Creando variables con: 'var', 'let', 'const'

--- app.js
+++ app.js
@@ -1,5 +1,45 @@
-/* JavaScript File */ 
-let nombre = prompt( '¿Cuál es tú nombre?' ),
-    edad   = prompt( '¿Cuál es tú edad?' );
+/* Var, Let y Const */ 
 
-document .getElementById( 'app' ) .innerHTML = `Bienvenido ${ nombre } de ${ edad } años.`;    
+/* Declaraciones con VAR */
+var edad;
+
+console .group( 'Declaraciones tipo VAR' );
+    var nombre = 'Juan';                    // Asignar valor
+    console .log( 'nombre ', nombre );      // nombre  Juan
+    var nombre = "Juanita";                 // Redeclarar la variable
+    console .log( 'nombre ', nombre );      // nombre  Juanita
+    nombre = 'Juliana';                     // Reasignar valor
+    console .log( 'nombre ', nombre );      // nombre  Juliana
+    
+    console .log( 'edad ', edad );          // edad undefined
+console .groupEnd();
+
+/* Formas de nombrar variables 
+
+var primerNombre;       // Camel Case
+var primer_nombre;      // Underscore
+var PrimerNombre;       // Pascal Case
+var primernombre;       // NO RECOMENDADO
+*/
+
+/* Declaraciones con LET (Con Ambito) */
+let primerNombre = 'Carlos',
+    tituloLibro = 'Aprenda JavaScript en 100 lecciones';
+
+console .group( 'Declaraciones tipo LET' );
+    console .log( primerNombre + ' lee "' + tituloLibro + '"' );    
+    console .log( `${ primerNombre } lee "${ tituloLibro }"` );  // Usando 'Template Literals'
+console .groupEnd();
+//let primerNombre = 'Alejandro';     // ERROR: 'let' no permite redeclarar una variable como lo permite 'var'
+
+
+/* Declaraciones con CONST (Constante) */
+const producto = 'Libro';
+
+/* Es un ERROR hacer lo siguiente 
+const producto;
+producto = 'Libro';
+*/
+console .group( 'Declaraciones tipo CONST' );
+    console .log( 'producto', producto );
+console .groupEnd();
