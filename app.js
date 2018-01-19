ed137b6c5249310f09686f6d5ed84bec8be48e4f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-19 13:29:50 -0500

    Algunos métodos disponibles para Números en JavaScript
    Algunos de los métodos son:
    
     - Math.PI
     - Math.round
     - Math.ceil
     - Math.floor
     - Math.sqrt
     - Math.abs
     - Math.pow
     - Math.min
     - Math.max
     - Math.random

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
-/* Métodos disponibles para 'Strings' en 'JavaScript' */ 
+/* Métodos disponibles para Números en 'JavaScript' */ 
 
-let tecnologia = 'JavaScript', 
-    mensaje    = "Aprendiendo JavaScript, CSS, HTML para ser un Frontend";
+const numero1 = 30,
+      numero2 = 20,
+      numero3 = 20.20,
+      numero4 = .1020,
+      numero5 = -3;
 
-console .group( 'Métodos disponibles para "Strings"' ); 
-    console .group( 'Aplicado a la variable "tecnologia"' ); 
-        console .log( 'concat()', tecnologia .concat( ' ', 'es genial!' ) );                // JavaScript es genial!
-        console .log( 'toUpperCase()', tecnologia .toUpperCase() );                         // JAVASCRIPT
-        console .log( 'toLowerCase()', tecnologia .toLowerCase() );                         // javascript
-        console .log( 'repeat( 10 )', tecnologia .repeat( 10 ) );                          // Repite 10 veces "JavaScript"
-    console .groupEnd();
-    console .group( 'Aplicado a la variable "mensaje"' );
-        console .log( 'indexOf( "JavaScript" ) ', mensaje .indexOf( 'JavaScript' ) );       // 12
-        console .log( 'indexOf( "PHP" ) ', mensaje .indexOf( 'PHP' ) );                     // -1
-        console .log( 'substring( 0, 11 ) ', mensaje .substring( 0, 11 ) );                 // Aprendiendo                                              
-        console .log( 'substring( 3, 21 ) ', mensaje .substring( 3, 21 ) );                 // endiendo JavaScrip
-        console .log( 'slice( 3, 21 ) ', mensaje .slice( 3, 21 ) );                         // endiendo JavaScrip (Similar a substring)
-        console .log( 'slice( -3 ) ', mensaje .slice( -3 ) );                               // end (de derecha a izquierda)
-        console .log( 'slice( -30 ) ', mensaje .slice( -30 ) );                             // CSS, HTML para ser un Frontend (de derecha a izquierda)
-        console .log( 'split( " " ) ', mensaje .split( ' ' ) );                             // [ "Aprendiendo", "JavaScript,", "CSS,", "HTML", "para", "ser", "un", "Frontend" ]           
-        console .log( 'split( " " ) ', mensaje .split( ',' ) );                             // [ "Aprendiendo JavaScript", " CSS", " HTML para ser un Frontend" ]
-        console .log( 'replace( "HTML", "PHP" )', mensaje .replace( 'HTML', 'PHP' ) );      // Aprendiendo JavaScript, CSS, PHP para ser un Frontend        
-        console .log( 'includes( "CSS" )', mensaje .includes( 'CSS' ) );                    // true (Si el texto 'CSS' esta incluido en el 'String')
-    console .groupEnd();
+console .group( 'Operaciones básicas' );
+    console .log( 'suma ', numero1 + numero2 );
+    console .log( 'resta ', numero1 - numero2 );
+    console .log( 'multiplicación ', numero1 * numero2 );
+    console .log( 'división ', numero1 / numero2 );
+    console .log( 'módulo ', numero1 % numero2 );
+    console .log( 'gerarquia de operadores ', ( ( 10 + 20 + 15 + 3 ) * .2 ) );
+console .groupEnd();    
+
+console .group( 'Operaciones con métodos de JavaScript' );
+    console .log( 'Math.PI ', Math .PI );                           // 3.1415... (Valor de PI)
+    console .log( 'Math.round( 2.562 ) ', Math .round( 2.562 ) );   // 3 (Redondea)
+    console .log( 'Math.ceil( 2.562 ) ', Math .ceil( 2.562 ) );     // 3 (Redondea por arriba)
+    console .log( 'Math.floor( 2.562 ) ', Math .floor( 2.562 ) );   // 2 (Redondea por abajo)
+    console .log( 'Math.sqrt( 144 ) ', Math .sqrt( 144 ) );         // 12 (Raíz cuadrada)
+    console .log( 'Math.abs( -3 ) ', Math .abs( numero5 ) );        // 3 (Número absoluto
+    console .log( 'Math.pow( 8, 3 ) ', Math .pow( 8, 3 ) );         // 512 (Potencia)
+    console .log( 'Math.min( 8, 3, 5, 6, 1, 7, 9, 8 ) ', Math .min( 8, 3, 5, 6, 1, 7, 9, 8 ) );     // 1 (El número minimo)
+    console .log( 'Math.max( 8, 3, 5, 6, 1, 7, 9, 8 ) ', Math .max( 8, 3, 5, 6, 1, 7, 9, 8 ) );     // 9 (El número máximo)         
+    console .log( 'Math.random() ', Math .random() );               // X.XXXX (Número aleatorio)
 console .groupEnd();    
