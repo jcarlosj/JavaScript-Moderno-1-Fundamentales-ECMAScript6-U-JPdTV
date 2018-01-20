e584e6fcbd9c5ffe9396a77031628902ca1c3b28
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-20 10:44:35 -0500

    Operadores de comparación en JavaScript

--- app.js
+++ app.js
@@ -1,32 +1,27 @@
-/* Tipos de datos en 'JavaScript' */ 
+/* Operadores de comparación en 'JavaScript' */ 
 
-let valor;
+const numero1 = 20,
+      numero2 = 50,
+      numero3 ='20';
 
-console .group( 'Tipos de datos Primitivos' );
-    console .log( 'valor ', typeof valor );                         // undefined
-    valor = 'Juan';
-    console .log( 'valor = "Juan" ', typeof valor );                // string
-    valor = 20;
-    console .log( 'valor = 20 ', typeof valor );                    // number
-    valor = -30;
-    console .log( 'valor = -30 ', typeof valor );                   // number
-    valor = -3.092;
-    console .log( 'valor = -3.092 ', typeof valor );                // number
-    valor = "-3.092";
-    console .log( 'valor = "-3.092" ', typeof valor );              // string
-    valor = true;
-    console .log( 'valor = true ', typeof valor );                  // boolean
-    valor = null;
-    console .log( 'valor = null ', typeof valor );                  // object
-    valor = Symbol( 'Simbolo' );
-    console .log( 'valor = Symbol( "Simbolo" ) ', typeof valor );   // symbol         
+console .group( 'Operadores de comparación' );
+    console .log( `${ numero1 } > ${ numero2 }`, numero1 > numero2 );   // false
+    console .log( `${ numero1 } < ${ numero2 }`, numero1 < numero2 );   // true
+
+    console .log( `${ numero1 } == ${ numero3 }`, numero1 == numero3 );     // true
+    console .log( `${ numero1 } === ${ numero3 }`, numero1 === numero3 );   // false
+
+    console .log( `${ numero1 } != ${ numero2 }`, numero1 != numero2 );   // true
+    
+    console .log( '"hola" == " hola"', 'hola' == ' hola' );     // false
+
+    /* (-) ABCDEFGHI... XYZabcdefg... xyz (+) */
+    console .log( '"a" > "b"', 'a' > 'b' );                     // false
+    console .log( '"Z" > "a"', 'Z' > 'a' );                     // false
+    console .log( '"z" > "A"', 'z' > 'A' );                     // true
+
+    console .log( 'null == undefined', null == undefined );     // true
+    console .log( 'null === undefined', null === undefined );   // false                   
 console .groupEnd();    
 
-console .group( 'Tipos de datos de Referencia' );
-    valor = [ 1, 2, 3, 4 ];
-    console .log( 'valor = [ 1, 2, 3, 4 ] ', typeof valor );        // object
-    valor = { nombre: 'Juan', profesion: 'Desarrollador' };
-    console .log( 'valor = { nombre: "Juan", profesion: "Desarrollador" }', typeof valor );        // object
-    valor = new Date();
-    console .log( 'valor = new Date() ', typeof valor );            // object  
-console .groupEnd();    
+/* ===  es un comparador estricto */
