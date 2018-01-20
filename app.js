a482cb21a6d5af2e3c9c4ddf49dec152f283b443
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-20 10:28:18 -0500

    Tipos de dato en JavaScript

--- app.js
+++ app.js
@@ -1,29 +1,32 @@
-/* Métodos disponibles para Números en 'JavaScript' */ 
+/* Tipos de datos en 'JavaScript' */ 
 
-const numero1 = 30,
-      numero2 = 20,
-      numero3 = 20.20,
-      numero4 = .1020,
-      numero5 = -3;
+let valor;
 
-console .group( 'Operaciones básicas' );
-    console .log( 'suma ', numero1 + numero2 );
-    console .log( 'resta ', numero1 - numero2 );
-    console .log( 'multiplicación ', numero1 * numero2 );
-    console .log( 'división ', numero1 / numero2 );
-    console .log( 'módulo ', numero1 % numero2 );
-    console .log( 'gerarquia de operadores ', ( ( 10 + 20 + 15 + 3 ) * .2 ) );
+console .group( 'Tipos de datos Primitivos' );
+    console .log( 'valor ', typeof valor );                         // undefined
+    valor = 'Juan';
+    console .log( 'valor = "Juan" ', typeof valor );                // string
+    valor = 20;
+    console .log( 'valor = 20 ', typeof valor );                    // number
+    valor = -30;
+    console .log( 'valor = -30 ', typeof valor );                   // number
+    valor = -3.092;
+    console .log( 'valor = -3.092 ', typeof valor );                // number
+    valor = "-3.092";
+    console .log( 'valor = "-3.092" ', typeof valor );              // string
+    valor = true;
+    console .log( 'valor = true ', typeof valor );                  // boolean
+    valor = null;
+    console .log( 'valor = null ', typeof valor );                  // object
+    valor = Symbol( 'Simbolo' );
+    console .log( 'valor = Symbol( "Simbolo" ) ', typeof valor );   // symbol         
 console .groupEnd();    
 
-console .group( 'Operaciones con métodos de JavaScript' );
-    console .log( 'Math.PI ', Math .PI );                           // 3.1415... (Valor de PI)
-    console .log( 'Math.round( 2.562 ) ', Math .round( 2.562 ) );   // 3 (Redondea)
-    console .log( 'Math.ceil( 2.562 ) ', Math .ceil( 2.562 ) );     // 3 (Redondea por arriba)
-    console .log( 'Math.floor( 2.562 ) ', Math .floor( 2.562 ) );   // 2 (Redondea por abajo)
-    console .log( 'Math.sqrt( 144 ) ', Math .sqrt( 144 ) );         // 12 (Raíz cuadrada)
-    console .log( 'Math.abs( -3 ) ', Math .abs( numero5 ) );        // 3 (Número absoluto
-    console .log( 'Math.pow( 8, 3 ) ', Math .pow( 8, 3 ) );         // 512 (Potencia)
-    console .log( 'Math.min( 8, 3, 5, 6, 1, 7, 9, 8 ) ', Math .min( 8, 3, 5, 6, 1, 7, 9, 8 ) );     // 1 (El número minimo)
-    console .log( 'Math.max( 8, 3, 5, 6, 1, 7, 9, 8 ) ', Math .max( 8, 3, 5, 6, 1, 7, 9, 8 ) );     // 9 (El número máximo)         
-    console .log( 'Math.random() ', Math .random() );               // X.XXXX (Número aleatorio)
+console .group( 'Tipos de datos de Referencia' );
+    valor = [ 1, 2, 3, 4 ];
+    console .log( 'valor = [ 1, 2, 3, 4 ] ', typeof valor );        // object
+    valor = { nombre: 'Juan', profesion: 'Desarrollador' };
+    console .log( 'valor = { nombre: "Juan", profesion: "Desarrollador" }', typeof valor );        // object
+    valor = new Date();
+    console .log( 'valor = new Date() ', typeof valor );            // object  
 console .groupEnd();    
