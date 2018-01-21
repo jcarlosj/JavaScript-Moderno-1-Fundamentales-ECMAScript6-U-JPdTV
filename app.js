5882734d0ae759b73a1175c4fd08dac567fb5a49
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-21 18:34:34 -0500

    Tipos de definición de Funciones en JavaScript
     - Tradicional
     - Función como variable (Se asigna como variable o constante)
     - Función IIFE (Se invoca inmediatamente se declara)
     - Función como método de propiedad (Método de un Objeto)

--- app.js
+++ app.js
@@ -1,26 +1,68 @@
-/* Funciones Propias creando Parámetros con valores por 'Default' en JavaScript */
+/* Tipos de definición de Funciones en JavaScript */
 
-/* Forma 1: */ 
-function saludarA( nombre ) {
-    /* Crear valor por defecto para un parámetro */
-    if( typeof nombre === 'undefined' ) {
-        nombre = 'Fulanito de Tal';
+/* Forma 1: Tradicional */
+function sumarNumeros( numero1 = 3, numero2 = 4 ) {
+    return numero1 + numero2;
+}
+
+console .group( 'Funcion Tradicional' );
+    console .log( 'suma ', sumarNumeros( 3, 4 ) );
+    console .log( 'suma ', sumarNumeros( 5 ) );
+console .groupEnd();
+
+/* Forma 2: Asignando la función como variable o constante */
+const suma = function( numero1 = 3, numero2 = 4 ) {
+    return numero1 + numero2;
+}
+
+console .group( 'Funcion como variable' );
+    console .log( 'suma ', suma( 3, 4 ) );
+    console .log( 'suma ', suma( 5 ) );
+console .groupEnd();
+
+/* Forma 3: IIFE (Se declaran e invocan inmediatamente) */
+( function() {
+    console .log( 'Creando un IIFE' );
+})();
+
+( function( tecnologia ) {
+    console .log( `Aprendiendo ${ tecnologia }` );
+})( 'JavaScript' );
+
+( function( numero1, numero2 ) {
+    console .group( 'Funcion IIFE' );
+        console .log( 'suma ', numero1 + numero2 );
+    console .groupEnd();
+})( 10, 9 );
+
+/* Forma 4: Métodos de Propiedad 
+   Cuando una función se pone dentro de un Objeto, estas funciones con conocidas como Métodos */
+
+const musica = {
+    canciones: [ 
+        'Alguien (Kany Garcia)',
+        'Arroyito (Fonseca)',
+        'Robarte un beso (Carlos vives)',
+        'Lo mejor que hay en mi vida (Andrés Cepeda)' 
+    ],
+    reproducir: function( id ) {
+        console .log( `Reproduciendo... ${ this .canciones[ id ] }` );
+    },
+    pausar: function() {
+        console .log( 'Pausar música' );
     }
-    console .log( `Hola ${ nombre }`);
 }
-/* Realizamos el llamado */
-saludarA( 'Maria' );
-saludarA( 'José' );
-saludarA( 'Jorge' );
-saludarA();
+
+/* Los métodos también se pueden crear desde afuera del Objeto */
+musica .buscar = function ( id ) {
+    console .log( `Busqueda... ${ this .canciones[ id ] }` );
+}
+
+// Llamada del método dentro del objeto
+console .group( 'Método de Propiedad (Función dentro de un Objeto)' );
+    musica .reproducir( 2 );
+    musica .pausar();
+    musica .buscar( 3 );
+console .groupEnd();
 
 
-/* Forma 2: */ 
-function saluda( nombre = 'Perencejo' ) {       /* Crear valor por defecto para un parámetro */   
-    console .log( `Hola ${ nombre }`);
-}
-/* Realizamos el llamado */
-saluda( 'Elisa' );
-saluda( 'Juliana' );
-saluda( 'Laura' );
-saluda();
