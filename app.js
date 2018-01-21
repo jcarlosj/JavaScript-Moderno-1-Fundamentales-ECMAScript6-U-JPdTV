0094edcb034bdca6186ce895fc5c470466c75c18
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-21 17:58:51 -0500

    Funciones Propias en JavaScript

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-/* Array de Objetos en JavaScript */
+/* Funciones Propias en JavaScript */
 
-const moviles = [
-    { modelo: 'S3' , marca: 'Samsung', color: 'blanco' },
-    { modelo: 'S4' , marca: 'Samsung', color: 'negro' },
-    { modelo: 'iPhone6' , marca: 'Apple', color: 'blanco' },
-    { modelo: 'G5' , marca: 'Motorola', color: 'dorado' }
-];
+/* Se define funcion (Sin Argumentos) */
+function saludar() {
+    console .log( 'Hola ');
+}
+/* Realizamos el llamado */
+saludar();
+saludar();
 
-console .log ( moviles );
-console .log ( 'Cantiad de moviles ', moviles .length );
-console .log ( 'Modelo del movil de la posición 1 ', moviles[ 1 ] .modelo );
+/* Se define funcion (Con Argumentos) */
+function saludarA( nombre ) {
+    console .log( `Hola ${ nombre }`);
+}
+/* Realizamos el llamado */
+saludarA( 'Maria' );
+saludarA( 'José' );
+saludarA( 'Jorge' );
 
-console .group( 'Recorrer cada Objeto del Array' );
-    /* Recorrer usando un loop for */
-    for( let i = 0; i < moviles .length; i++ ) {
-        console .log( `${ moviles[ i ] .marca } - ${ moviles[ i ] .modelo }` );
-    }
-console .groupEnd();
+/* Se define funcion (Con Argumentos que imprime valores) */
+function sumar1( numeroA, numeroB ) {
+    console .log( 'sumar1 ', numeroA + numeroB );
+}
+/* Realizamos el llamado */
+sumar1( 13, 9 );
+sumar1( 0, -1 );
 
-/* El Array de Objetos no puede ser cambiado por otro pues fue definido como 
-   constante (const), pero sus valores internos si 
-   
-moviles = [
-    { modelo: 'S8' , marca: 'Samsung', color: 'blanco' },
-    { modelo: 'Zend 2' , marca: 'Asus', color: 'negro' }
-];
-*/
-
-moviles[ 0 ] .modelo = 'Zend 3';
-moviles[ 0 ] .marca = 'Asus';
-moviles[ 0 ] .color = 'Negro';
-   
-console .log ( moviles );
+/* Se define funcion (Con Argumentos que retorna valores) */
+function sumar2( numeroA, numeroB ) {
+    return numeroA + numeroB;
+}
+/* Realizamos el llamado */
+console .log( 'sumar2 ', sumar2( 2, 3 ) );
+console .log( 'sumar2 ', sumar2( 10, 4 ) );
