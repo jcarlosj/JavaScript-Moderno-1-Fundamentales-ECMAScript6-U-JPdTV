f650cd0c5fccd19c3a3dc12bf650b257e3bc242f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-22 14:47:42 -0500

    Iteracciones (forEach Loop Function)

--- app.js
+++ app.js
@@ -1,31 +1,33 @@
-/* Iteraciones (Do-While Loop) 
-   El código se ejecuta al menos una ves sin importar si la condición se cumple o no */
-   let indice;
+/* Iteraciones (Foreach Loop) */
+const tareas = [
+    'Comer Cliente nuevo Proyecto',
+    'Viajar a Medellin',
+    'Estudiar Angular',
+    'Trabajar Proyecto WordPress',
+    'Investigar Despliegue en AWS'
+];
+console .log( tareas );
 
-   console .group( 'Do-While Loop' );
-   
-       console .group( 'Ejemplo 1' );
-           /* Ejemplo 1 */
-           indice = 0;
-   
-           do {
-               console .log( `Número ${ indice }` );
-   
-               indice++;
-           } while ( indice < 10 );
-   
-       console .groupEnd();
-   
-       console .group( 'Ejemplo 2' );    
-           /* Ejemplo 2 */
-           indice = 1000;
-           
-           do {
-               console .log( `Número ${ indice }` );
-   
-               indice++;
-           } while ( indice < 10 );
-   
-       console .groupEnd();    
-   
-   console .groupEnd();
+/* Forma 1: Usando FOR */
+console .group( 'Usando FOR' );
+    for( let i = 0; i < tareas .length; i++ ) {
+        console .log( tareas[ i ] );
+    }
+console .groupEnd();
+
+/* Forma 2: Usando WHILE */
+let i = 0;
+
+console .group( 'Usando WHILE' );
+    while( i < tareas .length ) {
+        console .log( tareas[ i ] );
+        i++;
+    }
+console .groupEnd();
+
+/* Forma 3: Usando FOREACH */
+console .group( 'Usando FOREACH' );
+    tareas .forEach( function( tareas, index ) {
+       console .log( `${ index } : ${ tareas }` ); 
+    });
+console .groupEnd();
