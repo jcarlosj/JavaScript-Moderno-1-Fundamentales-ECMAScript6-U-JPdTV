ecbde8dae67e4d44b2c677c12d106dffad680263
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-22 11:17:42 -0500

    Estructuras de control (if/else)

--- app.js
+++ app.js
@@ -1,38 +1,41 @@
-/* Fechas en JavaScript 
-   Es un tema muy extenso pero se verá las funciones básicas mas utilizadas 
-   En JavaScript existe un Objeto llamado Date */
+/* Estructuras de control en JavaScript (If/Else) */
 
-console .group( 'Fechas en JavaScript' );
+/* Ejemplo 1 */ 
+const edad = 17;
 
-    /* Fecha Actual */
-    const diaHoy = new Date();
-    console .log( 'Fecha actual ', diaHoy );
-    
-    /* Fecha específica: Mes, Dia y Año */
-    let navidad_2017 = new Date( '2017-12-25' );
-    console .log( 'Fecha Navidad 2017 ', navidad_2017 );
+if( edad >= 18 ) {
+    console .log( 'Eres mayor de edad puedes entrar al sitio' );
+}
+else {
+    console .log( 'Eres menor de edad NO puedes entrar al sitio' );
+}
 
-    /* Obtener Fechas */
-    let valor = diaHoy .getMonth();     /* Mes actual: El mes 0 es Enero y así los siguientes */
-    console .log( 'Mes actual ', valor );      
-    valor = diaHoy .getDate();          /* Dia actual */
-    console .log( 'Día actual ', valor );       
-    valor = diaHoy .getDay();           /* Dia actual */
-    console .log( 'Día actual ', valor );       
-    valor = diaHoy .getFullYear();      /* Año Actual */
-    console .log( 'Año actual ', valor );  
-    valor = diaHoy .getHours();         /* Hora Actual */
-    console .log( 'Hora actual ', valor );  
-    valor = diaHoy .getMinutes();       /* Minuto Actual */
-    console .log( 'Minuto actual ', valor );  
-    valor = diaHoy .getTime();          /* Todos los milisegundos que se han registrado desde 1970 */
-    console .log( 'Todos los milisegundos que se han registrado desde 1970 ', valor );  
+/* Ejemplo 2: Comprobar que una variable tiene un valor X */ 
+let puntaje;
 
-    /* Cambiar Fechas obtenidas */
-    valor = diaHoy .getFullYear();          
-    console .log( 'Año actual ', valor );
-    diaHoy .setFullYear( 2016 );        /* Cambia el año: igualmente se puede hacer para todos los metodos GET */ 
-    valor = diaHoy .getFullYear();
-    console .log( 'Año cambiado ', valor ); 
-console .groupEnd();
+/* Forma 1 */ 
+if( puntaje ) {
+    console .log( `El puntaje fue de ${ puntaje }` );
+}
+else {
+    console .log( `No hay puntaje`);
+}
 
+/* Forma 2 */
+if( typeof puntaje != 'undefined' ) {
+    console .log( `El puntaje fue de ${ puntaje }` );
+}
+else {
+    console .log( `No hay puntaje`);
+}
+
+/* Ejemplo 3: Comprobar dos valores */
+let efectivo = 500,
+    totalCarrito = 300;
+
+if( efectivo > totalCarrito ) {
+    console .log( 'Pago correcto' );
+}
+else {
+    console .log( 'Fondos insuficientes!' );
+}    
