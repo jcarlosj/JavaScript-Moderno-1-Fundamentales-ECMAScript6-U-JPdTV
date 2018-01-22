6f54aca0ee257833be248ef971cb1f84b4b4a27f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-22 12:08:01 -0500

    Estructuras de control (switch)

--- app.js
+++ app.js
@@ -1,14 +1,65 @@
-/* Operador ternario en JavaScript (Es como un If en una sola línea) */
+/* Estructuras de control en JavaScript (switch) */
 
 /* Ejemplo 1 */
-const loguedo = true;
-let estado;
+const metodoPago = 'bitcoin';
 
-estado = loguedo === true ? 'Si se logueo' : 'No se logueo';    /* (Condición) ? 'Si se cumple' : 'Si no se cumple'; */ 
-
-console .log( estado );
+switch ( metodoPago ) {
+    case 'efectivo':
+        console .log( `El usuario pago en ${ metodoPago }` );
+        break;
+    case 'cheque':
+        console .log( `El usuario pago con ${ metodoPago }` );
+        break;    
+    case 'debito':
+        console .log( `El usuario pago con tarjeta ${ metodoPago }` );
+        break;  
+    case 'credito':
+        console .log( `El usuario pago con tarjeta ${ metodoPago }` );
+        break;   
+    default: 
+        console .log( `${ metodoPago } método de pago no soportado.` );         
+}
 
 /* Ejemplo 2 */
-const cantidadHuevos = 11;
+let mes;
 
-console .log( cantidadHuevos == 12 ? 'Es una docena' : 'No es una docena', 'de huevos' );
+switch( new Date() .getMonth() ) {
+    case 0:
+        mes = 'Enero';
+        break;
+    case 1:
+        mes = 'Febrero';
+        break;
+    case 2:
+        mes = 'Marzo';
+        break;
+    case 3:
+        mes = 'Abril';
+        break;
+    case 4:
+        mes = 'Mayo';
+        break;
+    case 5:
+        mes = 'Junio';
+        break;
+    case 6:
+        mes = 'Julio';
+        break;
+    case 7:
+        mes = 'Agosto';
+        break;
+    case 8:
+        mes = 'Septiembre';
+        break;
+    case 9:
+        mes = 'Octubre';
+        break;
+    case 10:
+        mes = 'Noviembre';
+        break;
+    case 11:
+        mes = 'Diciembre';
+        break;                
+}
+
+console .log( `Mes actual: ${ mes }` );
