e46d01be7aeb293dae98adf4517ae323482a3090
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-21 20:03:35 -0500

    Fechas en JavaScript (Fundamentales)

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
-/* Manejando Errores en JavaScript */
+/* Fechas en JavaScript 
+   Es un tema muy extenso pero se verá las funciones básicas mas utilizadas 
+   En JavaScript existe un Objeto llamado Date */
 
-/* Manejando ERROR usando TryCatch o TryCatchFinallly*/
-try {
-    funcionQueNoExiste();       // Llama función que no existe
-} 
-catch( error ) {
-    console .log( error );
-}
-finally {
-    console .log( 'No le importa, ejecuta de todos modos' );
-}
+console .group( 'Fechas en JavaScript' );
 
-/* Podemos llamar la funcion primero */
-obtenerClientes();
+    /* Fecha Actual */
+    const diaHoy = new Date();
+    console .log( 'Fecha actual ', diaHoy );
+    
+    /* Fecha específica: Mes, Dia y Año */
+    let navidad_2017 = new Date( '12-25-2017' );
+    console .log( 'Fecha Navidad 2017 ', navidad_2017 );
 
-/* Definirla después */
-function obtenerClientes() {
-    console .log( 'Descargando... ' );     // Simulamos la descarga de los clientes
+    /* Obtener Fechas */
+    let valor = diaHoy .getMonth();     /* Mes actual: El mes 0 es Enero y así los siguientes */
+    console .log( 'Mes actual ', valor );      
+    valor = diaHoy .getDate();          /* Dia actual */
+    console .log( 'Día actual ', valor );       
+    valor = diaHoy .getDay();           /* Dia actual */
+    console .log( 'Día actual ', valor );       
+    valor = diaHoy .getFullYear();      /* Año Actual */
+    console .log( 'Año actual ', valor );  
+    valor = diaHoy .getHours();         /* Hora Actual */
+    console .log( 'Hora actual ', valor );  
+    valor = diaHoy .getMinutes();       /* Minuto Actual */
+    console .log( 'Minuto actual ', valor );  
+    valor = diaHoy .getTime();          /* Todos los milisegundos que se han registrado desde 1970 */
+    console .log( 'Todos los milisegundos que se han registrado desde 1970 ', valor );  
 
-    setTimeout( function() {
-        console .log( 'Listado de clientes Completo' );
-    }, 3000 );
-}
+    /* Cambiar Fechas obtenidas */
+    valor = diaHoy .getFullYear();          
+    console .log( 'Año actual ', valor );
+    diaHoy .setFullYear( 2016 );        /* Cambia el año: igualmente se puede hacer para todos los metodos GET */ 
+    valor = diaHoy .getFullYear();
+    console .log( 'Año cambiado ', valor ); 
+console .groupEnd();
 
-/* NOTA: No se debe abusar de TRY/CATCH no se debe usar para todo, pero
-         se sugiere usarlo en funciones en los que se esperan datos y no 
-         se saben si tienen datos o puedan llegar vacias */ 
