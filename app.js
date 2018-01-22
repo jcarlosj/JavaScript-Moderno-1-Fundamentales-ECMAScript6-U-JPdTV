70250dff5d37a1315b8e56112d22d673d4e4e2e9
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-22 11:54:47 -0500

    Operador ternario (Estructuras de control)

--- app.js
+++ app.js
@@ -1,48 +1,14 @@
-/* Operadores AND y OR en las estructuras de control en JavaScript (If/ElseIf) */
+/* Operador ternario en JavaScript (Es como un If en una sola línea) */
 
-/* Operador && (AND) */
-let edadAdmitida = 13,
-    generoAdmitido = 'masculino';
+/* Ejemplo 1 */
+const loguedo = true;
+let estado;
 
-if( edadAdmitida >= 18 && generoAdmitido == 'masculino' ) {
-    console .log( 'Debe prestar servicio militar, Bienvenido!' );
-}
-else if( generoAdmitido == 'masculino' ) {
-    console .log( 'Debe esperar a cumplir la mayoría de edad' );
-}
-else {
-    console .log( 'Las chicas no prestan servicio militar en Colombia!' );
-}
+estado = loguedo === true ? 'Si se logueo' : 'No se logueo';    /* (Condición) ? 'Si se cumple' : 'Si no se cumple'; */ 
 
-/* Operador || (OR) */
-let efectivo = 300,
-    credito = 300,
-    disponible = efectivo + credito;
-    totalCarrito = 500;
+console .log( estado );
 
-/* Forma 1 */    
-if ( totalCarrito < efectivo || totalCarrito < credito ) {
-    console .log( 'Puede pagar' );
-}    
-else {
-    console .log( 'No puede pagar' );
-}
+/* Ejemplo 2 */
+const cantidadHuevos = 11;
 
-/* Forma 2 */
-if ( totalCarrito < efectivo || totalCarrito < credito ) {
-    console .log( 'Puede pagar' );
-}    
-else if( totalCarrito < disponible ) {
-    console .log( 'Puede pagar (crédito + efectivo)' );
-}
-else {
-    console .log( 'No puede pagar' );
-}
-
-/* Forma 3 */
-if ( totalCarrito < efectivo || totalCarrito < credito || totalCarrito < disponible ) {
-    console .log( 'Puede pagar' );
-}    
-else {
-    console .log( 'No puede pagar' );
-}
+console .log( cantidadHuevos == 12 ? 'Es una docena' : 'No es una docena', 'de huevos' );
