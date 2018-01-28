0902ba35ab5b9dcc95622ff608d39b08aff49df7
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-28 14:46:35 -0500

    Método Alternativo (Objetos antes de ES6)

--- app.js
+++ app.js
@@ -1,22 +1,26 @@
-/* Literal Object */
-const cliente = {
-    nombre : 'Juan',
-    saldo : 200,
-    tipoCliente  : function() {
+/* Método Alternativo (Antes de ES6) */
+function Cliente( nombre, saldo ) {         
+    this .nombre = nombre;
+    this .saldo = saldo;
+    this .tipoCliente = function() {
         let tipo;
 
-        if(  this .saldo > 1000 ) {
+        if( this .saldo > 1000 ) {
             tipo = 'gold';
         }
-        else if( this .saldo > 5000 ) {
-            tipo = 'diamond'
+        else if( this .saldo > 500 ) {
+            tipo = 'platinum';
         }
         else {
-            tipo = 'classic';
+            tipo = 'normal';
         }
 
         return tipo;
     }
 }
 
-console .log( cliente .tipoCliente() );
+const cliente1 = new Cliente( 'Pablo', 12000 );  // --> El Constructor
+const cliente2 = new Cliente( 'Karen', 4000 );  // --> El Constructor
+
+console .log( `${ cliente1 .nombre } es cliente tipo ${ cliente1 .tipoCliente() }` );
+console .log( `${ cliente2 .nombre } es cliente tipo ${ cliente2 .tipoCliente() }` );
