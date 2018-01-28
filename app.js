2aaedff24c864dd78e333871f59f4b21cb00d973
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-28 14:21:49 -0500

    Literal Objects

--- app.js
+++ app.js
@@ -1,42 +1,22 @@
-/* Scope en JavaScript 
-   Se refiere al alcance que pueden tener las variables, 
-   funciones en los diferentes ámbitos (Su visibilidad) */
+/* Literal Object */
+const cliente = {
+    nombre : 'Juan',
+    saldo : 200,
+    tipoCliente  : function() {
+        let tipo;
 
-var a = 'a';
-let b = 'b';
-const c = 'c';
-
-/* Scope de la función */
-console .group( 'Scope en JavaScript' );
-
-    function funcionScope() {
-        var a = 'A';
-        let b = 'B';
-        const c = 'C';
-        console .log( `FUNCIÓN  (a, b, c): ${ a }, ${ b }, ${ c }` );   /* Su Scope (visibilidad) es dentro de la función */
-    }
-    funcionScope();
-
-    /* Scope de Bloque */
-    if( true ) {
-        var a = 'AA';
-        let b = 'BB';
-        const c = 'CC';
-        console .log( `BLOQUE   (a, b, c): ${ a }, ${ b }, ${ c }` );   /* Su Scope (visibilidad) es dentro un bloque */
-    }
-
-    /* FOR */
-    console .group( 'FOR' );
-    
-        for( var a = 0; a < 5; a++ ) {
-            console .log( 'FOR (a) ', a );
-        } 
-        for( let b = 0; b < 5; b++ ) {
-            console .log( 'FOR (b) ', b );
+        if(  this .saldo > 1000 ) {
+            tipo = 'gold';
         }
-    
-    console .groupEnd();
+        else if( this .saldo > 5000 ) {
+            tipo = 'diamond'
+        }
+        else {
+            tipo = 'classic';
+        }
 
-    console .log( `GLOBALES (a, b, c): ${ a }, ${ b }, ${ c }` );   /* Su Scope (visibilidad) es global, pueden en todo momento. No están dentro de bloques, funciones o métodos */
+        return tipo;
+    }
+}
 
-console .groupEnd();
+console .log( cliente .tipoCliente() );
