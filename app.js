1f7515d7a79dec35f438be18817b217721b844e8
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-28 18:38:29 -0500

    Herencia de Prototypes en JavaScript

--- app.js
+++ app.js
@@ -1,61 +1,25 @@
-/* Prototypes */
+/* Herencia de Prototypes en JavaScript */
 function Cliente( nombre, saldo ) {
     this .nombre = nombre;
     this .saldo = saldo;
-    this .tipoCliente = function() {            // ---> Esta restringido. Solo está disponible para el Objeto 'Cliente'
-        let tipo;
-
-        if( this .saldo > 1000 ) {
-            tipo = 'gold';
-        }
-        else if( this .saldo > 500 ) {
-            tipo = 'platinum';
-        }
-        else {
-            tipo = 'normal';
-        }
-
-        return tipo;
-    }
 }
 
-const cliente1 = new Cliente( 'Eva', 1100 ),
-      cliente2 = new Cliente( 'Sofia', 610 ),
-      cliente3 = new Cliente( 'Juan', 170 );
+function Empresa( nombre, saldo, telefono, tipo ) {
+    Cliente .call( this, nombre, saldo );       // ---> Hereda los atributos del objeto 'Cliente'
+    this .telefono = telefono;
+    this .tipoEmpresa = tipo;
+}
+Empresa .prototype = Object .create( Cliente .prototype .nombreClienteSaldo() );  // ---> Hereda el prototype (método) del objeto 'Cliente'
 
-console .log( cliente1 );
-console .log( cliente2 );
-console .log( cliente3 );
-
-/* Crea Prototype 'retirarSaldo' */
-Cliente .prototype .retirarSaldo = function( retiro ) {   // ---> Este método es más flexible, agrega a las funcionalidades al Objeto 'Cliente'
-    if( this. saldo > retiro ) {
-        return this .saldo -= retiro;
-    }
-    else {
-        return false;
-    }
+// Prototype para el Objeto 'Cliente'
+Cliente .prototype .nombreClienteSaldo = function() {
+    return `Nombre: ${ this .nombre }, Saldo: ${ this .saldo }`;
 }
 
-/* Crea Prototype 'getSaldo' */
-Cliente .prototype .getSaldoCliente = function() {
-    return `Nombre: ${ this .nombre }, Tu saldo disponible es: ${ this .saldo}, Tipo cliente: ${ this .tipoCliente() } `;
-}
+const cliente = new Cliente( 'Eva', 1100 );
+console .log( cliente );
+console .log( cliente .nombreClienteSaldo() );
 
-/* Resultados */
-console .group( 'Cliente 1' );
-    console .log( cliente1 .getSaldoCliente() );
-console .groupEnd();
-
-console .group( 'Cliente 2' );
-    console .log( cliente2 .getSaldoCliente() );
-    console .log( 'Retiro: 615, Saldo: ', cliente2 .retirarSaldo( 615 ) );
-console .groupEnd();
-
-console .group( 'Cliente 3' );
-    console .log( cliente3 .getSaldoCliente() );
-    console .log( 'Retiro: 95, Saldo: ', cliente3 .retirarSaldo( 95 ) );
-console .groupEnd();
-
-
-
+const empresa = new Empresa( 'Empresa de Acueducto', 1000000, 978734312, 'Estatal' );
+console .log( empresa );
+console .log( empresa .nombreClienteSaldo() );
