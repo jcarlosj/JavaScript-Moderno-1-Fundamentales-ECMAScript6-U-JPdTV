e475d0c1ebd414b1b7f099abf2e8c81a06330ee0
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-28 17:06:59 -0500

    Otros Objetos y Constructores

--- app.js
+++ app.js
@@ -1,26 +1,60 @@
-/* Método Alternativo (Antes de ES6) */
-function Cliente( nombre, saldo ) {         
-    this .nombre = nombre;
-    this .saldo = saldo;
-    this .tipoCliente = function() {
-        let tipo;
+/* Crear 'String' usando constructores */
+const nombre1 = 'Jerónimo',                 // Forma corriente
+      nombre2 = new String( 'Juliana' );    // Como constructor
 
-        if( this .saldo > 1000 ) {
-            tipo = 'gold';
-        }
-        else if( this .saldo > 500 ) {
-            tipo = 'platinum';
-        }
-        else {
-            tipo = 'normal';
-        }
+console .group( 'String' );
+    console .log( `${ nombre1 } es de tipo ${ typeof nombre1 }` );      // string
+    console .log( `${ nombre2 } es de tipo ${ typeof nombre2 }` );      // object
+console .groupEnd();
 
-        return tipo;
-    }
+/* Crear 'Number' usando constructores */
+const numero1 = 1,                  // Forma corriente
+      numero2 = new Number( 9 );    // Como constructor
+
+console .group( 'Number' );
+    console .log( `${ numero1 } es de tipo ${ typeof numero1 }` );      // number
+    console .log( `${ numero2 } es de tipo ${ typeof numero2 }` );      // object
+console .groupEnd();
+
+/* Crear 'Boolean' usando constructores */
+const boolean1 = false,                  // Forma corriente
+      boolean2 = new Boolean( true );    // Como constructor
+
+console .group( 'Boolean' );
+    console .log( `${ boolean1 } es de tipo ${ typeof boolean1 }` );      // boolean
+    console .log( `${ boolean2 } es de tipo ${ typeof boolean2 }` );      // object
+console .groupEnd();
+
+/* Crear un 'function' usando constructores */
+const funcion1 = function( numero1, numero2 ) {                         // Forma Corriente
+    return numero1 + numero2;
 }
+const funcion2 = new Function( 'numero1', 'numero2', 'return numero1 + numero2');   // Como constructor
 
-const cliente1 = new Cliente( 'Pablo', 12000 );  // --> El Constructor
-const cliente2 = new Cliente( 'Karen', 4000 );  // --> El Constructor
+console .group( 'Function' );
+    console .log( `${ funcion1( 2, 3 ) } es de tipo ${ typeof funcion1() }` );      // object
+    console .log( `${ funcion2( 3, 3 ) } es de tipo ${ typeof funcion2() }` );      // object
+console .groupEnd();
 
-console .log( `${ cliente1 .nombre } es cliente tipo ${ cliente1 .tipoCliente() }` );
-console .log( `${ cliente2 .nombre } es cliente tipo ${ cliente2 .tipoCliente() }` );
+/* Crear un 'object' usando constructores */
+const persona1 = {              // Forma corriente
+    nombre: 'Juan',
+    apellido: 'Jiménez'
+}
+const persona2 = new Object({ nombre: 'Juan', apellido: 'Jiménez' });   // Como constructor
+
+console .group( 'Object' );
+    console .log( `${ persona1 } es de tipo ${ typeof persona1 }` );      // object
+    console .log( `${ persona2 } es de tipo ${ typeof persona2 }` );      // object
+console .groupEnd();
+
+/* Crear un 'array' usando constructores */
+const arreglo1 = [ 1, 2, 3, 4 ],                // Forma corriente
+      arreglo2 = new Array( 1, 2, 3, 4 );       // Como constructor
+
+console .group( 'Array' );
+    console .log( `${ arreglo1 } es de tipo ${ typeof arreglo1 }` );      // object
+    console .log( `${ arreglo2 } es de tipo ${ typeof arreglo2 }` );      // object
+console .groupEnd();
+
+/* NOTA: Existen también expresiones regulares y otros */
