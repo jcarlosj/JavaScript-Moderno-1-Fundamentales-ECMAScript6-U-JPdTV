436c275847f1b7a43909364ba14fdb753b2e7c02
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-01-29 10:45:45 -0500

    Clases en JavaScript

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
-/* Object Create en JavaScript (POO en ES5) */
-const Cliente = {
-    imprimirSaldo : function() {
-        return `Nombre: ${ this .nombre }, Saldo: ${ this .saldo }`;
-    },
-    retirarSaldo : function( retiro ) {
+/* Clases en JavaScript (POO en ES6) */
+class Cliente {
+    constructor( nombre, apellido, saldo ) {
+        this .nombre = nombre;
+        this .apellido = apellido;
+        this .saldo = saldo;
+    }
+    /* Prototypes */
+    imprimirSaldo() {
+        return `Nombre: ${ this .nombre } ${ this .apellido }, Saldo: ${ this .saldo }`;
+    }
+    tipoCliente() {
+        let tipo;
+
+        if( this .saldo > 10000 ) {
+            tipo = 'gold';
+        }
+        else if( this .saldo > 5000 ) {
+            tipo = 'platinum';
+        }
+        else {
+            tipo = 'normal';
+        }
+
+        return tipo;
+    }
+    retirarSaldo( retiro ) {
         return this .saldo -= retiro;
     }
+    static bienvenida() {                               // ---> Prototype estático
+        return `Bienvenido al cajero.`;
+    }
+    
 }
 
-// Crear el Objeto usando 'Object Create'
-const mary = Object .create( Cliente );
-
-/*  Atributos  */
-mary .nombre = 'Sofia';
-mary .saldo = 900;
-
-/* Prototype (Método objeto Cliente) */
-mary .tipoCliente = function () {
-    let tipo;
-
-    if( this .saldo > 1000 ) {
-        tipo = 'gold';
-    }
-    else if( this .saldo > 500 ) {
-        tipo = 'platinum';
-    }
-    else {
-        tipo = 'normal';
-    }
-
-    return tipo;
-}
-
-console .log( mary );
-
-console .log( 'Retiro: 415, Saldo: ', mary .retirarSaldo( 415 ) );
-console .log( mary .imprimirSaldo() );
-console .log( 'Tipo Cliente', mary .tipoCliente() );
-
-/* NOTA: Es otra forma de crear un Objeto */
+const maria = new Cliente( 'Maria', 'Perez', 10000 );
+console .log( Cliente .bienvenida() );                  // ---> Los métodos (o prototypes) estáticos no requieren ser instanciados
+console .log( 'Tipo Cliente: ', maria .tipoCliente() );
+console .log( '> ', maria .imprimirSaldo() );
+console .log( 'Retira: 3000, Saldo ', maria .retirarSaldo( 7000 ) );
