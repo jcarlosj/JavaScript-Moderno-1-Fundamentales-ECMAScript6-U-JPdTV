3500b385ea68a465f1eaa249c325d2e319709d91
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-02-02 09:57:39 -0500

    CallBacks en JavaScript (Simulando REST API)

--- app.js
+++ app.js
@@ -2,24 +2,37 @@
  * Es una Función donde se ejecuta una función (forEach es un ejemplo de CallBack) 
  */
 
-const ciudades = [ 'Bogotá', 'Santiago de Cali', 'Medellín', 'Cartagena', 'Manizales' ]; 
+const paises = [ 'Colombia', 'México', 'Brasil', 'Argentina', 'Panamá', 'Canadá' ];
 
-console .group( 'Callback inline (forEach)' );
+// Función que implementa otra función como parámetro, Muestra nuevo pais agregado en un lapso de 4 segundos
+function nuevoPais( pais, mostrar ) {       // pais: valor / mostrar: función
+    console .log( 'Ejecuta nuevoPais()' );
+    setTimeout( function() {
+        paises .push( pais );
+        mostrar();                          // Hace referencia a la función que se pasa como parámetro mostrarPaises() en este caso
+    }, 4000 );      // 4seg
+}
 
-    // Es conocida como Inline Callback, ya que es una función anónima (Es la forma recomendada)
-    ciudades .forEach( function( ciudad ) {
-        console .log( '-', ciudad );
-    }); 
+// Función que muestra los paises en un lapso de tiempo de 2 seg
+function mostrarPaises() {
+    console .log( 'Ejecuta mostrarPaises()' );
+    // Simula traer los datos de un REST API
+    setTimeout( function() {
+        let html = '<ul>';      // Crea un "Template" vacío
 
-console .groupEnd();
+        /* Recorre el 'Array' de los países */
+        paises .forEach( function( pais ) {
+            html += `<li>${ pais }</li>`;    // Agrega los datos al Template
+        });
 
-console .group( 'CallBack Definido (forEach)' );
+        html += '</ul>';
 
-    // Es conocida como funciona definida, por que la función si tiene un nombre
-    function callback( ciudad ) {
-        console .log( '+', ciudad );
-    }
-    ciudades .forEach( callback );
+        // Agrega la plantilla con los datos al DOM
+        document .getElementById( 'app' ) .innerHTML = html;
 
-console .groupEnd();
+    }, 2000 );      // 2seg
+}
 
+// Ejecuta los llamados
+nuevoPais( 'Estados Unidos', mostrarPaises );  // Agregar nuevo país - Tarda 4 segundos (Pasa como parámetro la función mostrarPaises)
+mostrarPaises();                               // Mostrar los paises - Tarda 2 Segundos 
