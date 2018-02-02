b7d54f67471659eca7f24bb9bfbf31d775dee261
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-02-02 10:27:08 -0500

    Promises en JavaScript (reject)

--- app.js
+++ app.js
@@ -4,16 +4,21 @@
 // 'reject' se ejecuta cuando la Promise NO se cumple (Parámetro 2)
 
 // Creamos un Promise
-const esperando = new Promise( function ( resolve, reject ) {
-    // Simula tiempo que se puede tardar una REST API en leer unos 5000 registros por ejemplo
-    setTimeout( function() {
-        resolve( 'Se ejecutó' );        // Debe usarse el then para indicarle que hacer
-    }, 5000 );      // 5seg
-});        
+const aplicarDescuento = new Promise( function( resolve, reject ) {
+    const descuento = false;    // [true/false] Para simular o no el error 
 
-// Espera a que resolve se cumpla para ejecutar el código
-esperando .then( function( mensaje ) {
-    console .log( mensaje );
+    if( descuento ) {
+        resolve( 'Descuento Aplicado' );
+    }
+    else {
+        reject( 'No se puede aplicar el descuento' );
+    }
+
 });
 
-// NOTA: El valor que estamos revisando no está disponible en el momento, pero la idea es que esté disponible en el futuro
+/* Indicamos las acciones a realizar */
+aplicarDescuento .then( function( resultado ) {
+    console .log( 'CORRECTO', resultado );
+}). catch( function( error ) {
+    console .log( 'ERROR', error );
+});
