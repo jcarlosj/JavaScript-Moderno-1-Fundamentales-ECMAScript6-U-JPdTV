2539ee28d0875c319dc8422729e127fd5ab22bbe
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-02-03 09:49:33 -0500

    Arrow Functions, pasando parámetros

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
-/* Arrow Functions en JavaScript */
+/* Arrow Functions en JavaScript (Pasando Parámetros) */
 
 let aprendiendo;
 
 // --- FORMA TRADICIONAL ---
 // Pasar una función como variable
-aprendo1 = function() {
-    console .log( 'aprendo1() ', 'Aprendiendo JavaScript' );
+aprendo1 = function( tecnologia ) {
+    console .log( 'aprendo1() ', 'Aprendiendo ' + tecnologia );
 }
-aprendo1();
+aprendo1( 'JavaScript' );
 
 // --- FORMA NUEVA (Arrow Functions)
 // Pasar una función como variable 
-aprendo2 = () => {
-    console .log( 'aprendo2() ', 'Aprendiendo JavaScript' );
+aprendo2 = ( tecnologia ) => {
+    console .log( 'aprendo2() ', 'Aprendiendo ' + tecnologia );
 }
-aprendo2();
+aprendo2( 'JavaScript' );
 
 // Pasar una función como variable 
-aprendo3 = () => console .log( 'aprendo3() ', 'Aprendiendo JavaScript' );
-aprendo3();
+aprendo3 = ( tecnologia ) => console .log( 'aprendo3() ', 'Aprendiendo ' + tecnologia );
+aprendo3( 'JavaScript' );
 
 // Pasar una función como variable (retornando un valor)
-aprendo4 = () => 'Aprendiendo JavaScript';
-console .log( 'aprendo4() ', aprendo4() );
+aprendo4 = ( tecnologia ) => 'Aprendiendo ' + tecnologia;
+console .log( 'aprendo4() ', aprendo4( 'JavaScript' ) );
 
 // Pasar una función como variable (retornando un objeto)
-aprendo5 = () => ( { aprendiendo: 'Aprendiendo JavaScript' } );
-console .log( 'aprendo5() ', aprendo5() );
+aprendo5 = ( tecnologia ) => ( { aprendiendo: tecnologia } );
+console .log( 'aprendo5() ', aprendo5( 'JavaScript' ) );
+
+// NOTA: Cuando solo se pasa un parámetro se pueden obviar los paréntesis
+aprendo6 = tecnologia => ( { aprendiendo: tecnologia } );
+console .log( 'aprendo6() ', aprendo6( 'JavaScript' ) );
