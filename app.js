3a4f196767479bab8a1548139a06c35a2a5b318c
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-02-03 11:27:03 -0500

    Async/Await en JavaScript

--- app.js
+++ app.js
@@ -1,35 +1,38 @@
-/* Función 'forEach' con Arrow Functions en JavaScript */
-const productos = [ 'USB', 'Computadora', 'Pantalla', 'Teclado', 'Ratón' ];
+/* 'Async/Await' en JavaScript (ES7)
+     - Nos permite crear funciones que sean asíncronas, es decir que puedan ejecutarse junto a otras
+     - Como todo se ejecuta de forma asíncrona con 'await' le indicamos que cosas deben deternerse 
+       para ser ejecutadas hasta el final para luego continuar su ejecución
+     - Aún no está soportado en todos los navegadores
+     - Una función asíncrona siempre va a requerir un 'Promise'
+*/
 
-/* --- FORMA TRADICIONAL --- */ 
-
-// recorrer un 'Array' 
-console .group( 'forEach (Tradicional Functions)' );
-    productos .forEach( function( producto, index ) {
-        console .log( `${ index }. ${ producto }` );
+// Crea function asíncrona
+async function obtenerClientes() {
+    const clientes = new Promise( ( resolve, reject ) => {  // Crea un Promise (usando Arrow Functions)
+        // Simulamos la carga de datos desde un servidor
+        setTimeout( () => {                                 // Arrow Function
+            resolve( 'Clientes descargados...' );
+        }, 2000 );      // 2 seg
     });
-console .groupEnd();
 
-/* --- FORMA NUEVA --- */
+    // Simula si hubo o no un ERROR
+    const error = false;                                    // Cambiar valor para simularlo
 
-// usando ARROW FUNCTION para recorrer un 'Array' ---
-console .group( 'forEach (Arrow Functions)' );
-    productos .forEach( ( producto, index ) => {
-        console .log( `${ index }. ${ producto }` );
-    });
-console .groupEnd();
+    if( !error ) {
+        // 'await' Detiene la ejecución hasta que se cumpla el Promise que se le indica en este caso 'clientes'
+        const respuesta = await clientes;     
+        
+        return respuesta;
+    }
+    else {
+        // 'reject' no esta disponible aquí. Sin embargo podemos hacer que el 'Promise' envie un mensaje para el 'reject'
+        await Promise .reject( 'Hubo un error...' );
+    }
+}
 
-// usando ARROW FUNCTION para recorrer un 'Array' sin paréntesis (debe tener solo un parámetro)---
-console .group( 'forEach (Arrow Functions sin paréntesis)' );
-    let i = 0;
-    productos .forEach( producto => {
-        console .log( `${ i }. ${ producto }` );
-        i++;
-    });
-console .groupEnd();
+// Ejecutamos la función asíncrona
+obtenerClientes() 
+    .then( respuesta => console .log( 'OK: ', respuesta ) )     // 'respuesta' viene del 'resolve'
+    .catch( error => console .log( 'ERROR', error ) );          // 'error' ciene del 'reject' (Captura el ERROR)
 
-// usando ARROW FUNCTION para recorrer un 'Array' en una sola línea ---
-console .group( 'forEach (Arrow Functions en una línea)' );
-    i = 0;
-    productos .forEach( producto => { console .log( `${ i }. ${ producto }` ); i++; } );
-console .groupEnd();
+
