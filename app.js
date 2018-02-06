c6d2caa2b04f0f2c2398b86939bc00df5a75383f
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-02-06 09:21:23 -0500

    Destructuring de un 'Array' contenido en un objeto en JavaScript

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
-/* Destructuring 'Arrays' en JavaScript (ES6) 
+/* Destructuring 'Objects' y 'Arrays' en JavaScript (ES6) 
  *  Sirve para extraer valores de un Objeto, de un Arreglo 
  */
 
-const ciudades = [ 
-    'Londrés', 
-    'New York', 
-    'Montreal', 
-    'Sudáfrica', 
-    { 
-        idioma: 'Inglés' 
-    } 
-];
+ const cliente = {
+     tipo: 'Platinium',
+     saldo: 9000,
+     datos: {
+         nombre: 'Juliana',
+         apellido: 'Puerta',
+         residencia: {
+            ciudad: 'Medellín',
+            pais: 'Colombia'
+         }
+     },
+     movimientos: [ '12-03-2018', '09-02-2017', '21-11-2016' ]
+ }
 
-console .log( ciudades );
+ // Extraer datos usando Destructuring 
+let {
+    tipo, 
+    datos,
+    datos: { residencia }, 
+    movimientos: [ , dos, tres ]
+} = cliente;
 
-// Extrae los valores del Objeto contenido en el 'Array'
-[ idioma ] = ciudades;
-console .log( 'Idioma', idioma );               // Londrés
-
-[ , , , , lenguaje ] = ciudades;
-console .log( 'Idioma', lenguaje .idioma );     // Inglés
+console .log( 'Tipo:', tipo );                   // Usa 'tipo'
+console .log( 'Nombre:', datos .nombre );        // Usa 'datos'
+console .log( 'Apellido:', datos .apellido );    // Usa 'datos'
+console .log( 'País:', residencia .pais );       // Usa 'datos: { residencia }'
+console .log( 'Ciudad:', residencia .ciudad );   // Usa 'datos: { residencia }'
+console .log( 'Movimiento 2:', dos );            // Usa 'movimientos: [ , dos, tres ]'
+console .log( 'Movimiento 3:', tres );           // Usa 'movimientos: [ , dos, tres ]'
