71e15ddb6ac2ca8bb80d8959e60a5f8d811814fa
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-02-06 10:30:32 -0500

    Destructuring a 'Functions' en JavaScript, método ES6, parámetros del objeto por 'default'

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
 /* Destructuring a 'Functions' en JavaScript, método ES6 */
 
-// Crea función
-function reservacion( completo, opciones ) {
-    let { metodoPago, cantidad, dias } = opciones;      // Destructuring Método Nuevo (ES6)
+// Crea función pasando un objeto con valores predeterminados
+function reservacion( completo, { metodoPago = 'efectivo', cantidad = 0, dias = 0 } = {} ) {
 
     console .log( 'Método: ', metodoPago );
     console .log( 'Cantidad: ', cantidad );
-    console .log( 'Días: ', dias );    
+    console .log( 'Días: ', dias );  
+
+    if( completo ) {
+        console .log( 'Hacer la reserva' );
+    }
+    else {
+        console .log( 'Cancelar la reserva' );
+    }
+
 }
 
 // Llama reservación
-reservacion( 
-    true,                           // [true/false] Si se realiza la reservación
-    {                               // Objeto
-        metodoPago: 'credito',
-        cantidad: 2000,
-        dias: 3
-    } 
-); 
+console .group( 'Primera reservación' );
+    reservacion( 
+        false,                           // [true/false] Si se realiza la reservación
+        {}                               // Objeto vacío 
+    ); 
+console .groupEnd();
+
+console .group( 'Segunda reservación' );
+    reservacion( 
+        true,                           // [true/false] Si se realiza la reservación
+        {                               // Objeto 
+            cantidad: 5000,
+            dias: 5
+        }                               
+    ); 
+console .groupEnd();
