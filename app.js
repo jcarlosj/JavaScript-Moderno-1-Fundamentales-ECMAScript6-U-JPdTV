b4265df6bf16521f4a7d50695928127a35434a14
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-02-06 08:21:58 -0500

    Destructuring en JavaScript

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-/* Consumiendo REST API con FetchAPI usando 'Async/Await' en JavaScript (ES7) */
+/* Destructuring (ES6) 
+ *  Sirve para extraer valores de un Objeto, de un Arreglo 
+ */
 
-// Crea función asíncrona (Lee las tareas)
-async function leerTODOs () {
-    // FetchAPI usando Promises
-    const respuesta = await fetch( 'https://jsonplaceholder.typicode.com/todos' );      // 'await' bloquea la ejecución hasta que se conecte a los datos
-
-    // Procede cuando la respuesta esté lista
-    const datos = await respuesta .json();                                              // 'await' bloquea la ejecución hasta que obtenga el 'Promise', luego se le indica que retorne los datos como JSON
-
-    return datos;
+const cliente = {
+    nombre: 'Alejandra',
+    tipo: 'Premium'
 }
 
-console .log( 'leerTODOs', leerTODOs() );       // Promise
+console.log(cliente);
 
-leerTODOs()
-    .then( datos => console .log( 'OK!', datos ) )
-    .catch( error => console .log( 'ERROR', error ) );
+// Extracción de datos tradicional
+let nombre_1 = cliente .nombre,
+    tipo_1 = cliente .tipo;
+
+console .group( 'Extracción Tradicional' );
+    console .log( 'Nombre', nombre_1 );    
+    console .log( 'Tipo', tipo_1 );    
+console .groupEnd();
+
+// Extracción de datos usando Destructuring
+let { nombre, tipo } = cliente;       // Mapea los datos (por sus nombres, pues deben llamarse igual a los del Objeto) 
+
+console .group( 'Extracción usando Destructuring' );
+    console .log( 'Nombre', nombre );    
+    console .log( 'Tipo', tipo );    
+console .groupEnd();
