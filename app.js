49c263146fd989baa4759c1762195a1a330e529b
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-02-07 11:21:35 -0500

    Iteradores disponibles en JavaScript 'Default Iterator'

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-/* Iteradores disponibles en JavaScript 'Keys Iterator' (ES6) */
+/* Iteradores disponibles en JavaScript 'Default Iterator' (ES6) 
+ * Este tipo de iterador es cuando no le indicamos el método de iteración que se desea
+*/
 const ciudades = [ 'Bogotá', 'Cali', 'Medellín', 'Manizales', 'Cartagena' ],        // Declara un 'Array' con datos
       ordenes = new Set( [ 123, 231, 131, 102 ] )                                   // Declara un 'Set' con datos
       datos = new Map(                                                              // Declara un 'Map' con datos
@@ -14,25 +16,25 @@ console .group( 'Estructura de los datos' );
     console .log( datos ); 
 console .groupEnd();
 
-console .group( 'Keys Iterator' );
+console .group( 'Default Iterator' );
 
-    // Recorremos un 'Array' usando 'Keys Iterator' (Solo muestra las indices o llaves)
+    // Recorremos un 'Array' usando 'Default Iterator' (Solo muestra valores)
     console .group( 'Array "ciudades"' );
-    for( let ciudad of ciudades .keys() ) {             
+    for( let ciudad of ciudades ) {             
         console .log( ciudad );               
     }
     console .groupEnd();
 
-    // Recorremos un 'Set' usando 'Keys Iterator' (Solo muestra las indices o llaves)
+    // Recorremos un 'Set' usando 'Default Iterator' (Solo muestra valores)
     console .group( 'Set "ordenes"' );
-    for( let orden of ordenes .keys() ) {
+    for( let orden of ordenes ) {
         console .log( orden );                   
     }
     console .groupEnd();
 
-    // Recorremos un 'Map' usando 'Keys Iterator' (Solo muestra las indices o llaves)
+    // Recorremos un 'Map' usando 'Default Iterator' (Muestra el conjunto de datos en un 'Array', similar a el 'Entries Iterator')
     console .group( 'Map "datos"' );
-    for( let dato of datos .keys() ) {
+    for( let dato of datos ) {
         console .log( dato );                   
     }
     console .groupEnd();
