0742fd05eec233c3852a238495ed7a20db8e6c18
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2018-02-08 10:08:41 -0500

    Rangos en Expresiones Regulares en JavaScript

--- app.js
+++ app.js
@@ -1,22 +1,57 @@
-/* Negar Expresiones Regulares en JavaScript 
- * Para negar la expresión lo que hacemos es colocar símbolo ^ dentro de la expresión que vamos a negar */
+/* Rangos en Expresiones Regulares en JavaScript 
+ * - Para expresar un rango de números debe usarse las llaves, en las que se indica un 
+ *   número inicial y uno final para el rango. Ej: {1,2}, {3,9}
+ * - De igual manera se usan las llaves para establecer un valor fijo de caracteres que 
+ *   se desea evaluar. Ej: {4}, {9}, {1} */
 
 // ---- EJEMPLOS ----- 
 let valor, 
     expresionRegular;
 
-console .group( 'Negando Expresiones Regulares' );
+console .group( 'Rangos Expresiones Regulares' );
 
     // EJEMPLO 1:
-    console .group( 'Patrón NO existan 4 dígitos' );
+    console .group( 'Patrón fechas' );
         
         // PRIMERO
-        valor = '2018';
-        expresionRegular = /[^0-9]/;                // Todo lo contrario a los dígitos del 0 al 9
+        valor = '20-10-2018';
+        expresionRegular = /\d+-\d+-\d+/;                
 
         console .log( `valor: ${ valor } \nexpresión: ${ expresionRegular }` );
         console .log( 'Resultado: ', expresionRegular .test( valor ) );
 
+        // SEGUNDO
+        expresionRegular = /\d{1,2}-\d{1,2}-\d{4}/;                
+
+        console .log( `valor: ${ valor } \nexpresión: ${ expresionRegular }` );
+        console .log( 'Resultado: ', expresionRegular .test( valor ) );
+
+        // TERCERO
+        valor = '2-1-2018';                
+
+        console .log( `valor: ${ valor } \nexpresión: ${ expresionRegular }` );
+        console .log( 'Resultado: ', expresionRegular .test( valor ) );
+
+        // CUARTO
+        valor = '2-1-18';                
+
+        console .log( `valor: ${ valor } \nexpresión: ${ expresionRegular }` );
+        console .log( 'Resultado: ', expresionRegular .test( valor ) );
+
+        // QUINTO
+        valor = '2-100-2018';                
+
+        console .log( `valor: ${ valor } \nexpresión: ${ expresionRegular }` );
+        console .log( 'Resultado: ', expresionRegular .test( valor ) );
+
+        // SEXTO
+        valor = '200-10-2018';                
+
+        console .log( `valor: ${ valor } \nexpresión: ${ expresionRegular }` );
+        console .log( 'Resultado: ', expresionRegular .test( valor ) );
+
+        /* NOTA: { 1, 2 } Denota un rango de números (entre uno y dos dígitos)
+        *        { 4 }    Denota un número fijo de digitos */
     console .groupEnd();
 
 console .groupEnd();
